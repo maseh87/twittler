1c2e9b8d22c4af1c5ed64ba80553d1bca29a5ac8
Mason <maseh87@gmail.com>
2014-05-06 16:27:17 -0700

    organizing incoming tweets

--- app.js
+++ app.js
@@ -8,24 +8,32 @@
       $tweet.appendTo($ul);
     }
 $(document).ready(function(){
-
+  $('button').hide();
     $('li').addClass('animated bounceInLeft');
-    // $('button').hide();
-
   setInterval(function() {
+    var counter = 0;
     var $ul = $('.incomingTweets');
     $ul.html('');
     var index = streams.home.length;
     for(var i = 11; i < index; i++){
+      counter += 1;
       var tweet = streams.home[i];
       var $tweet = $('<li></li>');
       $tweet.text('@' + tweet.user + ': ' + tweet.message);
       $tweet.appendTo($ul);
+      if(counter > 30) {
+    $('button').addClass('animated bounceInLeft').show();
+  }
     }
-  }, 10000);
+  }, 5000);
+
+
   $('.incomingTweets').hide();
+
   $('button').on('click', function() {
     $('.incomingTweets').addClass('animated bounceInLeft').show();
+    $('button').hide();
   });
+
 });
 
