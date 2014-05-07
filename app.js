400f8658d95da5bce5b069d79ba94ba4bab59112
Mason <maseh87@gmail.com>
2014-05-07 16:43:37 -0700

    added button to update incoming tweets

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ $('.awaiting').hide();
 
 $(document).ready(function(){
   var count = 11;
+  var storedMessages = [];
+  $('.tweetButton').hide();
+  $('.tweetButton').removeClass('aimated tada');
   setInterval(function() {
     for(var i = count + 1; i < streams.home.length; i++) {
       count = i;
@@ -24,9 +27,21 @@ $(document).ready(function(){
       var time = tweet.created_at.toString();
       var hours = time.split(' ')[4];
       // $tweet.text('@' + tweet.user);
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + hours).addClass('animated fadeIn');
-      $tweet.prependTo($ul);
+      var tweets = $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + hours).addClass('animated fadeIn');
+      // $tweet.prependTo($ul);
+      storedMessages.push(tweets);
+      if(storedMessages.length > 15) {
+        $('.tweetButton').addClass('animated tada').show();
+
+      }
+
     }
   }, 10000);
-});
+
+  $('button').on('click', function() {
+    for(var i = 0; i < storedMessages.length; i++) {
+      storedMessages[i].prependTo($ul);
+    }
+  });
+;});
 
