c39c26fd8c8a0306c1b30768d6fb5ab49d645197
Mason <maseh87@gmail.com>
2014-05-07 02:01:09 -0700

    styled and set up.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
   var $ul = $('.initialTweets');
+  var $user = $('.user');
   $ul.html('');
   var index = streams.home.length;
     for(var i = 0; i < index; i++){
@@ -6,39 +7,14 @@
       var $tweet = $('<li></li>');
       var time = tweet.created_at.toString();
       var hours = time.split(' ')[4];
+      // $tweet.text('@' + tweet.user);
       $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + hours).addClass('animated fadeIn');
       $tweet.appendTo($ul);
     }
-
-
+$('.awaiting').hide();
 
 
 $(document).ready(function(){
-  $('button').hide();
-  setInterval(function() {
-    var counter = 0;
-    var $ul = $('.incomingTweets');
-    $ul.html('');
-    var index = streams.home.length;
-    for(var i = 11; i < index; i++){
-      counter += 1;
-      var tweet = streams.home[i];
-      var $tweet = $('<li></li>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($ul);
-      if(counter > 15) {
-    $('button').addClass('animated flip').show();
-  }
-    }
-  }, 5000);
-
-
-  $('.incomingTweets').hide();
-
-  $('button').on('click', function() {
-    $('.incomingTweets').addClass('animated bounceInLeft').show();
-    $('button').hide();
-  });
 
 });
 
