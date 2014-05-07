96a1f6ab88eca9cad7c17797822aaee6d516a24f
Mason <maseh87@gmail.com>
2014-05-07 02:44:39 -0700

    tweets on a timer

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ $('.awaiting').hide();
 
 
 $(document).ready(function(){
-
+  var count = 11;
+  setInterval(function() {
+    for(var i = count + 1; i < streams.home.length; i++) {
+      count = i;
+      var tweet = streams.home[i];
+      var $tweet = $('<li></li>');
+      var time = tweet.created_at.toString();
+      var hours = time.split(' ')[4];
+      // $tweet.text('@' + tweet.user);
+      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + hours).addClass('animated fadeIn');
+      $tweet.prependTo($ul);
+    }
+  }, 10000);
 });
 
