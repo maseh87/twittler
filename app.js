d7db78f3c3d3879d72c36ee8e7398a0a0133ff00
Mason <maseh87@gmail.com>
2014-05-10 15:37:47 -0700

    updates

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ $(document).ready(function(){
       var tweet = tweetArr[index];
       var $tweet = $('<li></li>');
       var time = tweet.created_at;
-      $tweet.html('<a> @ '+ tweet.user + ' </a>' + tweet.message + ' ' + '<span data-livestamp=" '+ tweet.created_at +' " ></span>');
+      $tweet.html('<a>@ '+ tweet.user + ' </a>' + tweet.message + ' ' + '<span data-livestamp=" '+ tweet.created_at +' " ></span>');
       $tweet.appendTo($ul);
       index -= 1;
     }
   }
+
+  $('input').on('keypress', function() {
+    console.log('yo');
+  });
+
 
   displayTweets(streams.home.length - 1, streams.home);
 
