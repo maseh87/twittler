794e2d1fa91369a868205c184802996afd5b04f7
Mason <maseh87@gmail.com>
2014-05-13 23:04:13 -0700

    added tweet button

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
+$('.twtSubmit').hide();
 $(document).ready(function(){
-  var $ul = $('.initialTweets');
-  var storedMessages = [];
 
-  $('.tweetButton').hide();
+  visitor = prompt('Tell me your name');
+  streams.users[visitor] = [];
 
-  var displayTweets = function(index, tweetArr) {
-    $ul.html('');
-    while(index >= 0) {
-      var tweet = tweetArr[index];
-      var $tweet = $('<li></li>');
-      var time = tweet.created_at;
-      $tweet.html('<a>@ '+ tweet.user + ' </a>' + tweet.message + ' ' + '<span data-livestamp=" '+ tweet.created_at +' " ></span>');
-      $tweet.appendTo($ul);
-      index -= 1;
+  var $ul = $('.initialTweets'),
+      storedMessages = [],
+      tweetContent;
+
+
+  // $('.tweetButton').hide();
+
+    var displayTweets = function(index, tweetArr) {
+      $ul.html('');
+      while(index >= 0) {
+        var tweet = tweetArr[index];
+        var $tweet = $('<li></li>');
+        var time = tweet.created_at;
+        $tweet.html('<a class="user">@ '+ tweet.user + ' </a>' + tweet.message + ' ' + '<span data-livestamp=" '+ tweet.created_at +' " ></span>');
+        $tweet.appendTo($ul);
+        index -= 1;
+      }
+    };
+
+  $('input').on('keyup', function() {
+    tweetContent = $('input').val();
+    console.log(tweetContent);
+    if(tweetContent.length >= 0) {
+      $('.twtSubmit').addClass('animated fadeIn').show();
     }
-  }
-
-  $('input').on('keypress', function() {
-    console.log('yo');
   });
 
+  $('.twtSubmit').on('click', function() {
+    writeTweet(tweetContent);
+    $('input').val('');
+  });
 
-  displayTweets(streams.home.length - 1, streams.home);
+  setInterval(function() {
+    displayTweets(streams.home.length - 1, streams.home);
+  }, 5000);
+
+
+  $('.user').on('click', function() {
+    console.log('you clicked');
+  });
+
 
 });
 
